2a90af59c81cee18e00c71e88261d1ee46c3e03b
Bitolinha <117381124+zatarana@users.noreply.github.com>
2026-04-19 12:50:52 -0300

    Update build.gradle.kts with new plugin versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,4 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
     id("com.google.devtools.ksp") version "1.9.24-1.0.20" apply false
     id("com.google.dagger.hilt.android") version "2.52" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "1.9.24" apply false
 }
